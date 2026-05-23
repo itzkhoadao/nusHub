7bab02c0beb2323a6f9b2c980ccfe2dd63548059
An Nguyen <annguyen15072017@gmail.com>
2026-05-24 03:47:44 +0700

    Implement user profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CreatePostPage from "./pages/CreatePostPage";
 import PostDetailPage from "./pages/PostDetailPage";
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/create-post" element={<CreatePostPage />} />
       <Route path="/posts/:id" element={<PostDetailPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 }
